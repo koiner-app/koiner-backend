7542f518e93c605c95f38e7c1c2e51a74201d012
Frank Weijers <frank@weijers.io>
2022-03-05 00:30:30 +0100

    Add BlockTransactionsResolver

---
+++ api/src/chain/api/graphql/block/query/block-transactions.resolver.ts
@@ -0,0 +1,27 @@
+import { Args, Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { SelectionSet } from '@appvise/graphql';
+import { BlockNode } from '@koiner/chain/api/graphql/block/dto/block.node';
+import { TransactionsRequest } from '@koiner/chain/api/graphql/transaction/dto/transactions.request';
+import { TransactionsConnection } from '@koiner/chain/api/graphql/transaction/dto/transactions.connection';
+import { TransactionsResolver } from '@koiner/chain/api/graphql/transaction/query/transactions.resolver';
+
+@Resolver((of) => BlockNode)
+export class BlockTransactionsResolver {
+  constructor(private transactionsResolver: TransactionsResolver) {}
+
+  @ResolveField((returns) => TransactionsConnection)
+  async transactions(
+    @Parent() parent: BlockNode,
+    @Args() request: TransactionsRequest,
+    @SelectionSet() selectionSet,
+  ): Promise<TransactionsConnection> {
+    if (!request.filter) {
+      request.filter = {};
+    }
+
+    // TODO: Make NumberFilter work
+    // request.filter.blockHeight = { equals: block.header.height.toString() };
+
+    return this.transactionsResolver.execute(request, selectionSet);
+  }
+}
