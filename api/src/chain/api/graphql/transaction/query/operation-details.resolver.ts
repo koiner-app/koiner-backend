b0b24e0abe1f05e5498f0e471905d7ec542d9f91
Frank Weijers <frank@weijers.io>
2022-03-12 22:33:39 +0100

    Resolve operation types SystemCallOperation + SystemContractOperation

--- api/src/chain/api/graphql/transaction/query/operation-details.resolver.ts
+++ api/src/chain/api/graphql/transaction/query/operation-details.resolver.ts
@@ -6,31 +6,49 @@ import { Krc20OperationNode } from '@koiner/contracts/api/graphql/krc20/dto/krc2
 import {
   ContractOperationReadRepository,
   OperationType,
+  SystemCallOperationReadRepository,
+  SystemContractOperationReadRepository,
   UploadContractOperationReadRepository,
 } from '@koiner/chain/domain';
 import { UploadContractOperationNode } from '@koiner/chain/api/graphql/operation/dto/upload-contract-operation.node';
 import { ContractOperationNode } from '@koiner/chain/api/graphql/operation/dto/contract-operation.node';
+import { SystemCallOperationNode } from '@koiner/chain/api/graphql/operation/dto/system-call-operation.node';
+import { SystemContractOperationNode } from '@koiner/chain/api/graphql/operation/dto/system-contract-operation.node';
 
 @Resolver((of) => OperationNode)
 export class OperationDetailsResolver {
   constructor(
-    private krc20OperationReadRepository: Krc20OperationReadRepository,
-    private uploadContractOperationReadRepository: UploadContractOperationReadRepository,
-    private operationReadRepository: ContractOperationReadRepository,
+    private readonly krc20OperationReadRepository: Krc20OperationReadRepository,
+    private readonly uploadContractOperationReadRepository: UploadContractOperationReadRepository,
+    private readonly operationReadRepository: ContractOperationReadRepository,
+    private readonly systemCallOperationRepository: SystemCallOperationReadRepository,
+    private readonly systemContractOperationReadRepository: SystemContractOperationReadRepository,
   ) {}
 
   @ResolveField((returns) => OperationDetailsUnion)
   async details(
     @Parent() parent: OperationNode,
   ): Promise<typeof OperationDetailsUnion> {
-    if (parent.type === OperationType.contractOperation) {
-      // TODO: Add OperationType.krc20Operation
+    if (parent.type === OperationType.systemCall) {
       try {
-        const operation = await this.krc20OperationReadRepository.findOneById(
+        const operation = await this.systemCallOperationRepository.findOneById(
           parent.id,
         );
 
-        return new Krc20OperationNode(operation);
+        return new SystemCallOperationNode(operation);
+      } catch (error) {
+        console.log('Error', error);
+      }
+    }
+
+    if (parent.type === OperationType.systemContractOperation) {
+      try {
+        const operation =
+          await this.systemContractOperationReadRepository.findOneById(
+            parent.id,
+          );
+
+        return new SystemContractOperationNode(operation);
       } catch (error) {
         console.log('Error', error);
       }
@@ -56,6 +74,19 @@ export class OperationDetailsResolver {
         );
 
         return new ContractOperationNode(operation);
+      } catch (error) {
+        console.log('Error', error);
+      }
+    }
+
+    if (parent.type === OperationType.contractOperation) {
+      // TODO: Add OperationType.krc20Operation
+      try {
+        const operation = await this.krc20OperationReadRepository.findOneById(
+          parent.id,
+        );
+
+        return new Krc20OperationNode(operation);
       } catch (error) {
         console.log('Error', error);
       }
