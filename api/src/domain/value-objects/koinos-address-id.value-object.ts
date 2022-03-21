a8672093401d17c348d16e4cab948a314e4243cf
Frank Weijers <frank@weijers.io>
2022-03-21 22:05:33 +0100

    Change address length check to between 33-34

--- api/src/domain/value-objects/koinos-address-id.value-object.ts
+++ api/src/domain/value-objects/koinos-address-id.value-object.ts
@@ -7,7 +7,7 @@ import {
 
 export class KoinosAddressId extends ID {
   protected validate({ value }: DomainPrimitive<string>): void {
-    if (!Guard.lengthIs(value, 34)) {
+    if (!Guard.lengthIsBetween(value, 33, 34)) {
       throw new ArgumentOutOfRangeException('address id is out of range');
     }
   }
