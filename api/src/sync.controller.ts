ef56b48505b9860d1d883e7216c7109a5bec22a3
Frank Weijers <frank@weijers.io>
2022-03-22 21:50:04 +0100

    Add extra sync route to simulate second intervals with GCP Scheduler

--- api/src/sync.controller.ts
+++ api/src/sync.controller.ts
@@ -9,4 +9,39 @@ export class SyncController {
   async sync(): Promise<void> {
     await this.worker.sync();
   }
+
+  @Get('/sync10')
+  async sync10(): Promise<void> {
+    setTimeout(async () => {
+      await this.worker.sync();
+    }, 10000);
+  }
+
+  @Get('/sync20')
+  async sync20(): Promise<void> {
+    setTimeout(async () => {
+      await this.worker.sync();
+    }, 20000);
+  }
+
+  @Get('/sync30')
+  async sync30(): Promise<void> {
+    setTimeout(async () => {
+      await this.worker.sync();
+    }, 30000);
+  }
+
+  @Get('/sync40')
+  async sync40(): Promise<void> {
+    setTimeout(async () => {
+      await this.worker.sync();
+    }, 40000);
+  }
+
+  @Get('/sync50')
+  async sync50(): Promise<void> {
+    setTimeout(async () => {
+      await this.worker.sync();
+    }, 50000);
+  }
 }
