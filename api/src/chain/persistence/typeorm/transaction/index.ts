4f155889f3ba4a602db01f56ba9802d43e1edb48
Frank Weijers <frank@weijers.io>
2022-03-25 23:09:18 +0100

    Remove export for moved file

--- api/src/chain/persistence/typeorm/transaction/index.ts
+++ api/src/chain/persistence/typeorm/transaction/index.ts
@@ -1,3 +1,2 @@
-export { OperationSchema } from '../operation/operation.schema';
 export { TransactionSchema } from './transaction.schema';
 export { TransactionSchemaFactory } from './transaction-schema.factory';
