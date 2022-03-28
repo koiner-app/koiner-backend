3587c39d7036cd41060ac157489ed17ee5175015
Frank Weijers <frank@weijers.io>
2022-03-28 20:35:53 +0200

    Remove blockHeight + search filter for Krc20Contracts

--- api/src/contracts/api/graphql/krc20/dto/krc20-contracts.filter.ts
+++ api/src/contracts/api/graphql/krc20/dto/krc20-contracts.filter.ts
@@ -4,24 +4,18 @@ import { StringFilterInput } from '@appvise/graphql';
 
 @InputType()
 export class Krc20ContractsFilter implements FilterType {
-  @Field((type) => StringFilterInput, { nullable: true })
-  search?: StringFilterInput;
-
-  @Field((type) => StringFilterInput, { nullable: true })
+  @Field(() => StringFilterInput, { nullable: true })
   name?: StringFilterInput;
 
-  @Field((type) => StringFilterInput, { nullable: true })
+  @Field(() => StringFilterInput, { nullable: true })
   symbol?: StringFilterInput;
 
-  @Field((type) => StringFilterInput, { nullable: true })
-  blockHeight?: StringFilterInput;
-
-  @Field((type) => [Krc20ContractsFilter], {
+  @Field(() => [Krc20ContractsFilter], {
     nullable: true,
   })
   AND?: Krc20ContractsFilter[];
 
-  @Field((type) => [Krc20ContractsFilter], {
+  @Field(() => [Krc20ContractsFilter], {
     nullable: true,
   })
   OR?: Krc20ContractsFilter[];
