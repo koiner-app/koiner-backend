e63c7d37a38080d7607fd3dda7939403bf0853dd
Frank Weijers <frank@weijers.io>
2022-04-16 01:55:28 +0200

    Krc20Balances still need to be updated

--- api/src/contracts/application/krc20/event/index.ts
+++ api/src/contracts/application/krc20/event/index.ts
@@ -1,17 +1,19 @@
 import { CreateKrcContractOnContractCreated } from './create-krc-contract-on-contract-created';
 import { CreateKrcOperationOnOperationCreated } from './create-krc-operation-on-operation-created';
 import { UpdateBlockRewardBalanceOnBlockRewardCreated } from './update-block-reward-balance-on-block-reward-created';
+import { UpdateKrc20BalanceOnBlockRewardCreated } from './update-krc20-balance-on-block-reward-created';
 import { UpdateKrc20BalancesOnOperationCreated } from './update-krc20-balances-on-operation-created';
 import { UpdateKrc20ContractOnKrc20BalanceCreated } from './update-krc20-contract-on-krc20-balance-created';
+import { UpdateKrc20ContractOnKrc20BalanceUpdated } from './update-krc20-contract-on-krc20-balance-updated';
 import { provideEventHandlers } from '@appvise/nestjs-utils';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { Logger } from '@appvise/domain';
 import { ContractStandardService } from '@koiner/contracts/application/contract-standard/service';
-import { UpdateKrc20ContractOnKrc20BalanceUpdated } from '@koiner/contracts/application/krc20/event/update-krc20-contract-on-krc20-balance-updated';
 
 export default [
   ...provideEventHandlers([
     UpdateBlockRewardBalanceOnBlockRewardCreated,
+    UpdateKrc20BalanceOnBlockRewardCreated,
     UpdateKrc20BalancesOnOperationCreated,
     UpdateKrc20ContractOnKrc20BalanceCreated,
     UpdateKrc20ContractOnKrc20BalanceUpdated,
