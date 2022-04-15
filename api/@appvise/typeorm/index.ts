2d5b4ca5d3cd18737a043a53b60e5aa73d01f4f8
Frank Weijers <frank@weijers.io>
2022-04-15 19:58:16 +0200

    Use @appvise/typeorm package

--- api/@appvise/typeorm/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './entity-schema.factory';
-export { EntityBaseSchema } from './entity-base.schema';
-export { QueryHelper } from './query-helper';
-export { TypeormReadRepository } from './typeorm.read.repository';
-export { TypeormRepository } from './typeorm.repository';
-export { TypeormWriteRepository } from './typeorm.write.repository';
-export { TypeormRepositoryProvider } from './repository.provider';
