b5738263e76b60d0d57d98a632a85335a2a5be10
Frank Weijers <frank@weijers.io>
2022-05-20 23:11:19 +0200

    Add contractStandardType to ContractNode

--- api/src/modules/contracts/api/graphql/contract/dto/contract.node.ts
+++ api/src/modules/contracts/api/graphql/contract/dto/contract.node.ts
@@ -1,15 +1,19 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { BaseNode } from '@appvise/graphql';
-import { Contract } from '@koiner/contracts/domain';
+import { Contract, ContractStandardType } from '@koiner/contracts/domain';
 
 @ObjectType('Contract')
 export class ContractNode extends BaseNode {
   @Field({ nullable: true })
   abi: string;
 
+  @Field(() => ContractStandardType, { nullable: true })
+  contractStandardType: ContractStandardType;
+
   constructor(entity: Contract) {
     super(entity);
 
     this.abi = entity.abi;
+    this.contractStandardType = entity.contractStandardType;
   }
 }
