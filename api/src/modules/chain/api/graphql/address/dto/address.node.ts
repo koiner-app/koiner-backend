80718364902ed48760aa1c1c43a99ec471f73348
Frank Weijers <frank@weijers.io>
2022-05-20 23:58:18 +0200

    Add isProducer field to AddressNode

--- api/src/modules/chain/api/graphql/address/dto/address.node.ts
+++ api/src/modules/chain/api/graphql/address/dto/address.node.ts
@@ -1,14 +1,15 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { BaseNode } from '@appvise/graphql';
 import { Address } from '@koiner/chain/domain';
-import { TransactionsConnection } from '@koiner/chain/api/graphql';
 
 @ObjectType('Address')
 export class AddressNode extends BaseNode {
-  @Field(() => TransactionsConnection)
-  transactions: TransactionsConnection;
+  @Field()
+  isProducer: boolean;
 
   constructor(entity: Address) {
     super(entity);
+
+    this.isProducer = entity.isProducer;
   }
 }
