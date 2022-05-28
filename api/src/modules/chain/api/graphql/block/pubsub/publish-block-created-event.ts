b39a796a5f5949639688964797654da3d6f05033
Frank Weijers <frank@weijers.io>
2022-05-28 10:09:24 +0200

    Listen to BlockRewardCreated to have producer included in subscription response

--- api/src/modules/chain/api/graphql/block/pubsub/publish-block-created-event.ts
+++ api/src/modules/chain/api/graphql/block/pubsub/publish-block-created-event.ts
@@ -1,21 +1,22 @@
 import { DomainEventHandler } from '@appvise/domain';
-import { Block, BlockCreated } from '@koiner/chain/domain';
+import { Block } from '@koiner/chain/domain';
 import { PubSubEngine } from '@koiner/pubsub-engine';
 import { QueryBus } from '@nestjs/cqrs';
 import { BlockQuery } from '@koiner/chain/application';
 import { BlockNode } from '@koiner/chain/api/graphql/block/dto/block.node';
+import { BlockRewardCreated } from '@koiner/contracts/domain';
 
 export class PublishBlockCreatedEvent extends DomainEventHandler {
   constructor(
     private readonly pubSub: PubSubEngine,
     private readonly queryBus: QueryBus,
   ) {
-    super(BlockCreated);
+    super(BlockRewardCreated);
   }
 
-  async handle(event: BlockCreated): Promise<void> {
+  async handle(event: BlockRewardCreated): Promise<void> {
     const block = await this.queryBus.execute<BlockQuery, Block>(
-      new BlockQuery(event.height),
+      new BlockQuery(event.blockHeight),
     );
 
     await this.pubSub.publish('blockCreated', {
