4e81fc67de110d0e86ba89879a246d81fc9ac97e
Frank Weijers <frank@weijers.io>
2022-08-29 00:49:11 +0200

    Also include EventCreatedMessage

---
+++ modules/chain/events/src/event/event-created.message.ts
@@ -0,0 +1,20 @@
+export class EventCreatedMessage {
+  static routingKey = 'chain.event.created';
+
+  constructor(props: Partial<EventCreatedMessage>) {
+    Object.assign(this, props);
+  }
+
+  readonly id!: string;
+  readonly parentId!: string;
+  readonly parentType!: string;
+  readonly sequence?: number;
+  readonly contractId?: string;
+  readonly name!: string;
+  readonly data?: string;
+  readonly impacted?: string[];
+
+  public toString(): string {
+    return JSON.stringify(this);
+  }
+}
--- modules/chain/events/src/index.ts
+++ modules/chain/events/src/index.ts
@@ -1,4 +1,5 @@
 export { AddressCreatedMessage } from './address/address-created.message';
 export { BlockCreatedMessage } from './block/block-created.message';
+export { EventCreatedMessage } from './event/event-created.message';
 export { OperationCreatedMessage } from './operation/operation-created.message';
 export { UploadContractOperationCreatedMessage } from './operation/upload-contract-operation-created.message';
