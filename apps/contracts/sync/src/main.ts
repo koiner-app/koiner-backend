0a518e995438fb6cefc1c02a23ec46397b1df347
Frank Weijers <frank@weijers.io>
2022-08-31 23:26:38 +0200

    Set error levels to log for sync services for now

--- apps/contracts/sync/src/main.ts
+++ apps/contracts/sync/src/main.ts
@@ -4,7 +4,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn'],
+  });
   const port = process.env.PORT || 3102;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
