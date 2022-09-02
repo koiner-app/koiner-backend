fdbe9e5b254cd42ab8791397f0f8d4465d7d0ced
Frank Weijers <frank@weijers.io>
2022-09-02 19:45:42 +0200

    Load rpcNodes from .env

--- modules/koinos/jsonrpc/src/koinos.config.ts
+++ modules/koinos/jsonrpc/src/koinos.config.ts
@@ -1,5 +1,7 @@
 // TODO: Load from app config file
 export const koinosConfig = {
-  rpcNodes: ['https://jsonrpc.dev.koiner.app'],
+  rpcNodes: process.env.KOINOS_RPC_NODES
+    ? JSON.parse(process.env.KOINOS_RPC_NODES)
+    : ['https://api.koinos.io'],
   signerSeed: 'seed',
 };
