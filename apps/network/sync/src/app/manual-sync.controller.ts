21fd42494901cd3e75bc8606d03332fd2c60bc91
Frank Weijers <frank@weijers.io>
2022-10-21 20:22:03 +0200

    Add reset endpoint to network sync

--- apps/network/sync/src/app/manual-sync.controller.ts
+++ apps/network/sync/src/app/manual-sync.controller.ts
@@ -9,7 +9,10 @@ import {
 import { SyncService } from './sync.service';
 import { koinos } from '../config';
 import { CommandBus } from '@nestjs/cqrs';
-import { UndoBlockRewardsCommand } from '@koiner/network/application';
+import {
+  UndoBlockRewardsCommand,
+  UndoBlockRewardsFromCheckpointCommand,
+} from '@koiner/network/application';
 
 @Controller()
 export class ManualSyncController {
@@ -39,6 +42,22 @@ export class ManualSyncController {
       await this.commandBus.execute(
         new UndoBlockRewardsCommand({
           blockHeights: input.heights,
+        })
+      );
+    } else {
+      throw new ForbiddenException();
+    }
+  }
+
+  @Post('/reset')
+  async reset(
+    @Query('secret') secret: string,
+    @Body() input: { checkPoint: number }
+  ): Promise<void> {
+    if (secret && koinos.syncSecret && secret === koinos.syncSecret) {
+      await this.commandBus.execute(
+        new UndoBlockRewardsFromCheckpointCommand({
+          checkPoint: input.checkPoint,
         })
       );
     } else {
