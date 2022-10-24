407475fe99a0b3418cd46027748620882eb43666
Frank Weijers <frank@weijers.io>
2022-10-24 21:28:01 +0200

    Process AddressUsed messages from network

---
+++ apps/chain/sync/src/app/amqp/subscribe/emit-network-address-used-queue-events.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { Logger } from '@appvise/domain';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { AddressUsedMessage } from '@koiner/chain/events';
+
+@Injectable()
+export class EmitNetworkAddressUsedQueueEvents {
+  constructor(
+    private readonly logger: Logger,
+    private readonly eventEmitter: EventEmitter2
+  ) {}
+  @RabbitSubscribe({
+    queueOptions: {
+      channel: 'koiner.chain.channel.address',
+    },
+    exchange: 'koiner.network.event',
+    routingKey: AddressUsedMessage.routingKey,
+    queue: 'koiner.chain.queue.address',
+  })
+  async handle(message: any): Promise<void> {
+    return new Promise((resolve) => {
+      const event = new AddressUsedMessage(JSON.parse(message));
+
+      this.eventEmitter
+        .emitAsync(AddressUsedMessage.routingKey, event)
+        .then(() => {
+          resolve();
+        })
+        .catch(() => {
+          this.logger.error(
+            'Could not process koiner.chain.queue.address message (from network)'
+          );
+          resolve();
+        });
+    });
+  }
+}
--- apps/chain/sync/src/app/amqp/subscribe/index.ts
+++ apps/chain/sync/src/app/amqp/subscribe/index.ts
@@ -1,11 +1,13 @@
 import { EmitChainAddressUsedQueueEvents } from './emit-chain-address-used-queue-events';
 import { EmitContractsAddressUsedQueueEvents } from './emit-contracts-address-used-queue-events';
+import { EmitNetworkAddressUsedQueueEvents } from './emit-network-address-used-queue-events';
 import { EmitTokenizeAddressUsedQueueEvents } from './emit-tokenize-address-used-queue-events';
 import { UpdateChainStatsFromChainEventsQueue } from './update-chain-stats-from-chain-events-queue';
 
 export const ChainAmqpSubscribeHandlers = [
   EmitChainAddressUsedQueueEvents,
   EmitContractsAddressUsedQueueEvents,
+  EmitNetworkAddressUsedQueueEvents,
   EmitTokenizeAddressUsedQueueEvents,
   UpdateChainStatsFromChainEventsQueue,
 ];
