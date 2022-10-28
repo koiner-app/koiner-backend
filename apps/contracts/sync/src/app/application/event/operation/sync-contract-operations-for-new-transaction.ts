a4d0cc4a3ae54f0814cd6a096123a93fdb1010fb
Frank Weijers <frank@weijers.io>
2022-10-28 22:05:05 +0200

    Don't save token operations in contract service but do publish them to amqp

--- apps/contracts/sync/src/app/application/event/operation/sync-contract-operations-for-new-transaction.ts
+++ apps/contracts/sync/src/app/application/event/operation/sync-contract-operations-for-new-transaction.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { UUID } from '@appvise/domain';
 import { RawBlocksService } from '@koinos/jsonrpc';
 import { TransactionCreatedMessage } from '@koiner/chain/events';
@@ -10,13 +11,16 @@ import {
   CreateContractOperationCommand,
 } from '@koiner/contracts/application';
 import { Contract } from '@koiner/contracts/domain';
+import { ContractStandardType } from '@koiner/contracts/standards';
+import { ContractOperationWithTokenTypeCreatedMessage } from '@koiner/contracts/events';
 
 @Injectable()
 export class SyncContractOperationsForNewTransaction {
   constructor(
     private readonly commandBus: CommandBus,
     private readonly queryBus: QueryBus,
-    private readonly rawBlocksService: RawBlocksService
+    private readonly rawBlocksService: RawBlocksService,
+    private readonly amqpConnection: AmqpConnection
   ) {}
 
   @OnEvent(`${TransactionCreatedMessage.routingKey}.operation_queue`, {
@@ -49,18 +53,37 @@ export class SyncContractOperationsForNewTransaction {
 
           contractStandardType = contract.contractStandardType;
 
-          await this.commandBus.execute(
-            new CreateContractOperationCommand({
-              id: UUID.generate().value,
-              blockHeight: event.blockHeight,
-              contractId,
-              transactionId: event.id,
-              entryPoint: operationJson.entry_point,
-              args: operationJson.args,
-              contractStandardType,
-              timestamp: event.timestamp,
-            })
-          );
+          const sharedProps = {
+            blockHeight: event.blockHeight,
+            contractId,
+            transactionId: event.id,
+            entryPoint: operationJson.entry_point,
+            args: operationJson.args,
+            contractStandardType,
+            timestamp: event.timestamp,
+          };
+
+          // Don't save token events. Publish message so contract service can process it
+          if (contractStandardType === ContractStandardType.token) {
+            const message = new ContractOperationWithTokenTypeCreatedMessage({
+              operationId: UUID.generate().value,
+              ...sharedProps,
+              publishedAt: Date.now(),
+            });
+
+            await this.amqpConnection.publish(
+              'koiner.contracts.event',
+              ContractOperationWithTokenTypeCreatedMessage.routingKey,
+              message.toString()
+            );
+          } else {
+            await this.commandBus.execute(
+              new CreateContractOperationCommand({
+                id: UUID.generate().value,
+                ...sharedProps,
+              })
+            );
+          }
         }
       }
     }
