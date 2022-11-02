e8743e3ad223a5c0230501e4db1882cc8f89bdd2
Frank Weijers <frank@weijers.io>
2022-11-02 23:34:22 +0100

    Add JSONRPC_CACHE_CLEANUP_AGE env var

--- apps/chain/sync/src/app/cron-sync.controller.ts
+++ apps/chain/sync/src/app/cron-sync.controller.ts
@@ -1,8 +1,9 @@
 import { Controller } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { CommandBus } from '@nestjs/cqrs';
+import { koinosConfig } from '@koinos/jsonrpc';
 import * as findRemoveSync from 'find-remove';
 import { StartSynchronizationBatchCommand } from '@koiner/sync/application';
-import { CommandBus } from '@nestjs/cqrs';
 
 @Controller()
 export class CronSyncController {
@@ -10,14 +11,20 @@ export class CronSyncController {
 
   @Cron(CronExpression.EVERY_5_SECONDS, { name: 'cronSync' })
   async cron(): Promise<void> {
-    await this.commandBus.execute(new StartSynchronizationBatchCommand());
+    await this.commandBus.execute(
+      new StartSynchronizationBatchCommand({ chainId: koinosConfig.chainId })
+    );
   }
 
   @Cron(CronExpression.EVERY_MINUTE, { name: 'cronCleanupCache' })
   async cleanupCache(): Promise<void> {
+    const cleanupAge = process.env.JSONRPC_CACHE_CLEANUP_AGE
+      ? parseInt(process.env.JSONRPC_CACHE_CLEANUP_AGE)
+      : 21600;
+
     // Delete cached block json files older than 20 minutes
     findRemoveSync(process.env.JSONRPC_CACHE_DIR ?? '/jsonrpc', {
-      age: { seconds: 1200 },
+      age: { seconds: cleanupAge },
       extensions: '.json',
       limit: 5000,
     });
