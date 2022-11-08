926f557c4c45e715729fe7946fc25c65de42145c
Frank Weijers <frank@weijers.io>
2022-11-08 23:37:54 +0100

    Set default values for Koinos contract ids

--- modules/koinos/jsonrpc/src/koinos.config.ts
+++ modules/koinos/jsonrpc/src/koinos.config.ts
@@ -5,9 +5,9 @@ export const koinosConfig = {
     : ['https://api.koinos.io'],
   signerSeed: 'seed',
   contracts: {
-    koin: process.env.KOIN_CONTRACT_ID,
-    vhp: process.env.VHP_CONTRACT_ID,
-    pob: process.env.POB_CONTRACT_ID,
+    koin: process.env.KOIN_CONTRACT_ID ?? '15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL',
+    vhp: process.env.VHP_CONTRACT_ID ?? '1AdzuXSpC6K9qtXdCBgD5NUpDNwHjMgrc9',
+    pob: process.env.POB_CONTRACT_ID ?? '159myq5YUhhoVWu3wsHKHiJYKPKGUrGiyv',
   },
   syncSecret: process.env.SYNC_SECRET,
 };
