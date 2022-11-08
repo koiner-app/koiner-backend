c60ea9e0821f7f0d0f4104858e7ad82ba8d6dc76
Frank Weijers <frank@weijers.io>
2022-11-08 23:31:55 +0100

    Load KoinosModule to include koilib Provider

--- apps/chain/sync/src/app/chain-sync.module.ts
+++ apps/chain/sync/src/app/chain-sync.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AmqpModule } from './amqp.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CqrsModule } from '@nestjs/cqrs';
-import { BlocksServiceModule } from '@koinos/jsonrpc';
+import { BlocksServiceModule, KoinosModule } from '@koinos/jsonrpc';
 import { SynchronizationModuleApplicationHandlers } from '@koiner/sync/application';
 import {
   SynchronizationModuleModels,
@@ -35,6 +35,7 @@ if (process.env.SYNC_CRON === 'active') {
     AmqpModule,
     CqrsModule,
     ScheduleModule,
+    KoinosModule,
     BlocksServiceModule,
     TypeOrmModule.forFeature(SynchronizationModuleModels),
     SyncLoggerModule,
