82fbab1d58a39a49ef6d29268859b6d0143845a8
Frank Weijers <frank@weijers.io>
2023-08-27 12:23:10 +0200

    Update VHP contract

--- modules/koinos/jsonrpc/src/koinos.config.ts
+++ modules/koinos/jsonrpc/src/koinos.config.ts
@@ -7,7 +7,7 @@ export const koinosConfig = {
   signerSeed: 'seed',
   contracts: {
     koin: process.env.KOIN_CONTRACT_ID ?? '15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL',
-    vhp: process.env.VHP_CONTRACT_ID ?? '1AdzuXSpC6K9qtXdCBgD5NUpDNwHjMgrc9',
+    vhp: process.env.VHP_CONTRACT_ID ?? '18tWNU7E4yuQzz7hMVpceb9ixmaWLVyQsr',
     pob: process.env.POB_CONTRACT_ID ?? '159myq5YUhhoVWu3wsHKHiJYKPKGUrGiyv',
   },
   syncSecret: process.env.SYNC_SECRET ?? 'secret',
