5b82385d0fe4ae60a455deee0308c5512e4df9f0
Frank Weijers <frank@weijers.io>
2023-08-27 12:22:36 +0200

    Stop logging batch started + completed events

--- modules/sync/logger/src/sync/synchronization/index.ts
+++ modules/sync/logger/src/sync/synchronization/index.ts
@@ -1,8 +1,6 @@
 import { provideEventHandlers } from '@koiner/nestjs-utils';
 import { AddEventLogOnStopSignaled } from './add-event-log-on-stop-signaled';
-import { AddEventLogOnSynchronizationBatchCompleted } from './add-event-log-on-synchronization-batch-completed';
 import { AddEventLogOnSynchronizationBatchFailed } from './add-event-log-on-synchronization-batch-failed';
-import { AddEventLogOnSynchronizationBatchStarted } from './add-event-log-on-synchronization-batch-started';
 import { AddEventLogOnSynchronizationBatchTimedOut } from './add-event-log-on-synchronization-batch-timed-out';
 import { AddEventLogOnSynchronizationCreated } from './add-event-log-on-synchronization-created';
 import { AddEventLogOnSynchronizationResumed } from './add-event-log-on-synchronization-resumed';
@@ -11,9 +9,7 @@ import { AddEventLogOnSynchronizationStopped } from './add-event-log-on-synchron
 export const LogSynchronizationAutomations = [
   ...provideEventHandlers([
     AddEventLogOnStopSignaled,
-    AddEventLogOnSynchronizationBatchCompleted,
     AddEventLogOnSynchronizationBatchFailed,
-    AddEventLogOnSynchronizationBatchStarted,
     AddEventLogOnSynchronizationBatchTimedOut,
     AddEventLogOnSynchronizationCreated,
     AddEventLogOnSynchronizationResumed,
